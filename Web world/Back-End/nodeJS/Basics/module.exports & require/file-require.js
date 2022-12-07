41dde9af8420948358e183f338b3e220c3ca1121
Muhannad Hurmiz <MoeHurmiz@gmail.com>
2022-12-07 21:15:57 +0300

    Added new skills

--- Web world/Back-End/nodeJS/Basics/module.exports & require/file-require.js
+++ Web world/Back-End/nodeJS/Basics/module.exports & require/file-require.js
@@ -6,16 +6,16 @@ To make require,
 Example:
 */
 
-var mod = require("./module.exports");
+var x = require("./module.exports");
 
 /* And the way to use it is as follows:
     Type the name of the variable that you put the require in,
      then put a dot and write the name you put when doing the export,
     Examples: */
 
-mod.dreamCar("R8 V10 Plus");
+x.dreamCar("R8 V10 Plus");
 
-mod.favoriteColor("red");
+x.favoriteColor("red");
 
 
 // To see the result run the current file in CMD
